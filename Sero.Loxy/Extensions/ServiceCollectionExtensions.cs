2252648301daf25e5b93889987a823b09cad6535
Agnael <oleg.kuzmych@outlook.com>
2022-06-30 03:22:02 -0300

    Improves multicontext support

--- Sero.Loxy/Extensions/ServiceCollectionExtensions.cs
+++ Sero.Loxy/Extensions/ServiceCollectionExtensions.cs
@@ -7,21 +7,29 @@ using System.Text;
 
 namespace Sero.Loxy
 {
-    public static class ServiceCollectionExtensions
-    {
-        public static IServiceCollection AddLoxy<TContext>(
-            this IServiceCollection services)
-            where TContext : class
-        {
-            services
-                .AddSingleton<ILoggerProvider, LoxyLoggerProvider>()
-                .AddSingleton<IExceptionMapper, DefaultExceptionMapper>()
-                .AddSingleton<IScopeBuilder, DefaultScopeBuilder>()
-                .AddSingleton<IEventMapper, DefaultEventMapper>()
-                .AddScoped<ILoxy<TContext>, Loxy<TContext>>()
-                .AddScoped<ILoxy>(sp => sp.GetService<ILoxy<TContext>>());
+   public static class ServiceCollectionExtensions
+   {
+      public static IServiceCollection AddLoxy<TContext>(this IServiceCollection services)
+         where TContext : class
+      {
+         services
+            .AddSingleton<ILoggerProvider, LoxyLoggerProvider>()
+            .AddSingleton<IExceptionMapper, DefaultExceptionMapper>()
+            .AddSingleton<IScopeBuilder, DefaultScopeBuilder>()
+            .AddSingleton<IEventMapper, DefaultEventMapper>()
+            .AddScoped<ILoxy<TContext>, Loxy<TContext>>()
+            .AddScoped<ILoxy>(sp => sp.GetService<ILoxy<TContext>>());
 
-            return services;
-        }
-    }
+         return services;
+      }
+
+      public static IServiceCollection AddLoxyContextSynonym<TContext, TSynonym>(this IServiceCollection services)
+         where TContext : class
+         where TSynonym : class, ILoxy<TContext>
+      {
+         services.AddScoped<TSynonym>(sp => (TSynonym)sp.GetService<ILoxy<TContext>>());
+
+         return services;
+      }
+   }
 }
