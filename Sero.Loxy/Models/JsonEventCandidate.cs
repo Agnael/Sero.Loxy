eb491c9e326c5ac5df41de7ef443a2ea57b6719e
Agnael <oleg.kuzmych@outlook.com>
2022-09-27 00:18:09 -0300

    Haphazardly adds an extension point for the JsonEventCandidate, to be able to configure the serializer.

--- Sero.Loxy/Models/JsonEventCandidate.cs
+++ Sero.Loxy/Models/JsonEventCandidate.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Functional.Maybe.Json;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System;
 using System.Collections;
@@ -6,47 +7,53 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 
-namespace Sero.Loxy
+namespace Sero.Loxy;
+
+public record JsonEventCandidate<TState>(
+   LogLevel Level, 
+   string Category, 
+   string Message, 
+   TState State, 
+   Exception Exception = null
+) : EventCandidate(Level, Category, Message, Exception)
 {
-   public record JsonEventCandidate<TState>(
-      LogLevel Level, 
-      string Category, 
-      string Message, 
-      TState State, 
-      Exception Exception = null
-   ) : EventCandidate(Level, Category, Message, Exception)
+   public static readonly JsonSerializerSettings JsonSerializerSettings;
+
+   static JsonEventCandidate()
    {
+      JsonSerializerSettings = new JsonSerializerSettings();
+      JsonSerializerSettings.Converters.Add(new MaybeConverter());
+   }
 
-      private string GetSerialized(object obj)
+   private string GetSerialized(object obj)
+   {
+      StringBuilder sb = new StringBuilder();
+      using (StringWriter sw = new StringWriter(sb))
+      using (JsonTextWriter writer = new JsonTextWriter(sw))
       {
-         StringBuilder sb = new StringBuilder();
-         using (StringWriter sw = new StringWriter(sb))
-         using (JsonTextWriter writer = new JsonTextWriter(sw))
-         {
-            writer.QuoteChar = '\'';
+         writer.QuoteChar = '\'';
 
-            JsonSerializer ser = new JsonSerializer();
-            ser.Serialize(writer, obj);
-         }
-
-         return sb.ToString();
+         JsonSerializer ser = JsonSerializer.Create(JsonSerializerSettings);
+         ser.Serialize(writer, obj);
       }
 
-      public override IEnumerable<string> GetDetails()
+      return sb.ToString();
+   }
+
+   public override IEnumerable<string> GetDetails()
+   {
+      if (this.State is IEnumerable)
       {
-         if (this.State is IEnumerable)
+         List<string> detailList = new List<string>();
+
+         foreach (object item in (this.State as IEnumerable))
          {
-            List<string> detailList = new List<string>();
-
-            foreach (object item in (this.State as IEnumerable))
-            {
-               detailList.Add(GetSerialized(item));
-            }
-
-            return detailList;
+            detailList.Add(GetSerialized(item));
          }
 
-         return new string[] { GetSerialized(this.State) };
+         return detailList;
       }
+
+      return new string[] { GetSerialized(this.State) };
    }
 }
